6b7f9aaf7b725b51f5d2468e47bedac37dddcad3
Craig Brunengraber <brunengraber@hotmail.com>
2019-03-01 14:46:07 -0500

    data in useable form

--- pathfinder.py
+++ pathfinder.py
@@ -1,12 +1,16 @@
+        ### step 1 how to sort data into two lists x axis and y axis - should these be defined?
 class Map:
     """
     Reads text file of map, and puts into multistring form. Shows elevation by color in image file.
     """
     def __init__(self):
+        
+
 
         with open('elevation_small.txt') as file:
-            elevation_small = file.read().split() 
-        print(elevation_small)
+            elevations = [line.split() for line in file]
+            elevations = [[int(elevation) for elevation in row] for row in elevations]
+            print(elevations)
 
 class MapImage:
     """
@@ -17,4 +21,5 @@ class Pathfinder:
     """
     Shows the path starting from 0 0 to the easternside of the map. Create a method for a "greedy" algorithm that takes a "step" to any of the three adjacent cells in the next column over. Must choose closest to current elevation, in case of a tie go forwards if possible, and in a case of smallest change is a tie, pick randomly.
     """
+
 
